744bfead206c500fcbf187d1e4cbbc02155abb10
nikhilpatil1204 <nikhilpatil7967@gmail.com>
2025-07-05 22:36:31 +0530

    Add files via upload

---
+++ Day3/Dowhileloop.java
@@ -0,0 +1,15 @@
+package Day3;
+
+public class Dowhileloop {
+	
+	public static void main (String[] args) {
+		
+		int i=0;
+		do {
+			System.out.println("Welcone");
+			i++;
+		}
+		while(i < 5);
+	}
+	
+}
